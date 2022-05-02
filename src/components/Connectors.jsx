ad3d32947d9738f5c1295adcf349f5af952590cd
eacent <eacentcheung@gmail.com>
2022-05-02 16:59:55 +0800

    add Connectors Profile

---
+++ src/components/Connectors.jsx
@@ -0,0 +1,55 @@
+import { Provider, chain, createClient } from 'wagmi'
+import { CoinbaseWalletConnector } from 'wagmi/connectors/coinbaseWallet'
+import { InjectedConnector } from 'wagmi/connectors/injected'
+import { MetaMaskConnector } from 'wagmi/connectors/metaMask'
+import {WalletConnectConnector } from 'wagmi/connectors/walletConnect'
+
+import { Profile  } from './Profile.jsx';
+
+const alchemyId = process.env.ALCHEMY_ID
+
+const chains = [chain.mainnet, chain.ropsten, chain.polygon]
+const defaultChain = chain.mainnet
+
+// Set up connectors
+const client = createClient({
+    autoConnect: true,
+    connectors({ chainId }) {
+        const chain = chains.find((x) => x.id === chainId) ?? defaultChain
+        const rpcUrl = chain.rpcUrls.alchemy 
+            ? `${chain.rpcUrls.alchemy}/${alchemyId}` 
+            : chain.rpcUrls.mainnet
+        return [
+            new MetaMaskConnector({ chains }),
+            new CoinbaseWalletConnector({ 
+                chains,
+                options: {
+                    appName: 'wagmi',
+                    chainId: chain.id,
+                    jsonRpcUrl: rpcUrl,
+                }
+            }),
+            new WalletConnectConnector({
+                chains,
+                options: {
+                    qrcode: true,
+                    rpc: { [chain.id]: rpcUrl },
+                }
+            }),
+            new InjectedConnector({ 
+                chains,
+                options: { name: 'Injected' },
+             }),
+        ]
+    }
+})
+
+function App() {
+    return (
+        <Provider client={client}>
+            <Profile />
+        </Provider>
+    )
+}
+
+export default App;
